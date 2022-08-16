83727c068b7e35017ca76983f29fa6c269fa0b86
Amey-Bura <111319476+Amey-Bura@users.noreply.github.com>
2022-08-15 20:34:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Bluebery Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+
+streamlit.text('hard Boiled Free-Range Egg')
